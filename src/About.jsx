7efdb91315cdf2b576eacc431c68d2a6a0cdf2d5
Addie Lopshire-Bratt <bratt.addie@gmail.com>
2023-09-01 13:26:54 -0600

    half of about page implemented

--- src/About.jsx
+++ src/About.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import { useNavigate } from 'react-router';
+
 const { aboutInfo } = require('./information/About.js');
+
 function About() {
+  const navigate = useNavigate();
+  navigate(0);
+
   return (
-    <div className="grid grid-cols-3 mx-10">
+    <div className="grid grid-cols-3 mx-10 p-10 gap-x-20">
       {/* <h2 className="text-center mb-4 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white pt-10">
         Under construction
       </h2> */}
       <div>
         {aboutInfo.softwareEngineer.map((paragraph) => {
           return (
-          <p className="mt-5">
+          <p className="mt-5 text-3xl">
             {paragraph}
           </p>
           )
         })}
+      </div>
+      <div>
+        <img className="rounded-full mb-5" src="https://avatars.githubusercontent.com/u/111605723?v=4"/>
+        <img className="rounded-full mb-5" src="https://lh3.googleusercontent.com/drive-viewer/AITFw-xq228I-KoX-kfMwQT8bXQxzCGFoHT44fxRGRpFcCZFayyLzPWBP1knp6bys5HaiHaPD7bv1uhY3Rqb5fTYYiPxb63Y9Q=s1600"/>
+        <img className="rounded-full" src="https://lh3.googleusercontent.com/drive-viewer/AITFw-yTDFxx1TwfuB3dLiYe7KDtlFBlYS8g-z-KRVz8S5KMpmpxijPQfGGpa0EczhllP4jiGq8ogtUrKwl0olOtONGH2yNvRg=s1600"/>
       </div>
     </div>
   )
--- src/information/About.js
+++ src/information/About.js
@@ -2,6 +2,7 @@ export const aboutInfo = {
   softwareEngineer: [
     "I have always been naturally good with computers, but only in the last two years did I start really diving in. Motivated by the dopamine rush of solving problems, I've found deep satisfaction in building websites, databases, and everything in between.",
     "My usual stack is JavaScript/Node.js, React, Express, and either MongoDB or MySQL. I also have experience in HTML/CSS, Python, and many different DBMS. I have experience across the full spectrum of software engineering: web development to system design--and I still can't choose a favorite!",
+    "I have served as the de facto tech support at every job I've had--no matter how technical the actual position. I discovered my love for technology and engineering when I was an intern with Starstruck entertainment. I would spend my shifts shadowing the tech lead while we modified, fixed, and built audio technology.",
     'As a relatively new engineer, I\'m looking to be a part of a team with strong mentorship opportunities where I can keep developing my skills to one day be a senior engineer. I\'m open to taking on projects on my own, but I\'m still keeping those fairly small-scale while I focus on getting a foot in the door of the industry. I have contributed to some open source projects, and I\'m continuing to look for similar opportunities. To see more about my achievements in software, check out the Software Projects portion of this site!'
   ]
 }
