534e8855a7b30a138cbe5a2648f828a21da2df0d
Addie Lopshire-Bratt <bratt.addie@gmail.com>
2023-12-19 14:58:19 -0800

    started mobile styling for navigation and welcome page

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 function NavBar() {
+  const screenWidth = screen.width;
   return (
     <div className="bg-gradient-to-r from-yellow-400 to-yellow-100 mx-10 p-10 sticky top-0 z-50">
       <Link to='/'>
-        <h1 className="mb-4 text-8xl font-extrabold leading-none tracking-tight text-dark-green md:text-5xl lg:text-6xl dark:text-white pt-10">
+        <h1 className="mb-4 lg:text-8xl md:text-6xl sm:text-6xl font-extrabold leading-none tracking-tight text-dark-green dark:text-white pt-10">
           Addie Lopshire-Bratt
         </h1>
       </Link>
-      <nav className="flex max-w-full content-start items-center justify-between flex-wrap text-xl text-dark-green border-b-4 border-dark-green">
+      {screenWidth >= 640 && <nav className="flex max-w-full content-start items-center justify-between flex-wrap text-xl text-dark-green border-b-4 border-dark-green">
         <Link to='/about'>
-          <button className="mr-20">About</button>
+          <button>About</button>
         </Link>
         <Link to='/software'>
-          <button className="mr-20">Software Projects</button>
+          <button>Software Projects</button>
         </Link>
         <Link to='/history'>
-          <button className="mr-20">Work History</button>
+          <button>Work History</button>
         </Link>
         <Link to='/art'>
-          <button className="mr-20">Art</button>
+          <button>Art</button>
         </Link>
         <Link to='/contact'>
           <button>Contact</button>
         </Link>
-      </nav>
+      </nav>}
+      {screenWidth < 640 && <FontAwesomeIcon className="float-right align-items-center" icon={faBars} />}
     </div>
   )
 }
--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-const { welcomeMessages } = require('./information/Welcome.js');
+import { welcomeMessages } from './information/Welcome.js';
 
 function Welcome() {
   // console.log(welcomeFile);
   return (
     <div className="mx-10 content-center grid place-items-center text-dark-green">
-      <h2 className="inline-block align-top motion-safe:animate-bounce text-center mb-4 text-9xl font-extrabold leading-none tracking-tight text-dark-green md:text-5xl lg:text-6xl dark:text-white pt-10">
+      <h2 className="inline-block align-top motion-safe:animate-bounce text-center mb-4 lg:text-9xl font-extrabold leading-none tracking-tight text-dark-green md:text-2xl sm:text-xl dark:text-white pt-10">
         Welcome
       </h2>
-      <div className="max-w-7xl grid grid-cols-2 gap-x-12">
-        <div className="text-center text-5xl max-w-2xl">
+      <div className="max-w-7xl sm:max-w-max grid grid-cols-2 gap-x-12 flex sm:flex-col">
+        <div className="text-center lg:text-5xl md:text-2xl lg:max-w-2xl sm:max-w-max">
           {welcomeMessages.intro}
         </div>
         {/* <ul className="mt-10 text-center text-2xl mb-5 max-w-md">
