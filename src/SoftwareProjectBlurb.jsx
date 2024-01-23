b2f15d8274c6dd82f7fcb0daffd2e1d98e555632
Addie Lopshire-Bratt <bratt.addie@gmail.com>
2024-01-23 13:26:54 -0700

    refactored software projects section

--- src/SoftwareProjectBlurb.jsx
+++ src/SoftwareProjectBlurb.jsx
@@ -13,13 +13,13 @@ function SoftwareProjectBlurb({ title, role, link, description, path }) {
       <h3 className="softwareBlurbTitle">{title}</h3>
       <p className="softwareBlurbRole">{role}</p>
       <p className="softwareBlurbDescription">{description}</p>
-      <div className="relative flex content-end justify-between">
-        <button type="button" className="border-2 border-orange text-lt-yellow bg-orange p-4 rounded-xl">
+      <div className="softwareBlurbBtnContainer">
+        <button type="button" className="button">
           <a target="_blank" rel="noreferrer" href={link}>Learn more</a>
         </button>
-        <button className="float-right border-2 border-orange rounded-xl p-4 text-lt-yellow bg-orange" type="button" onClick={toggleSize}>{isEnlarged ? "Hide" : "Expand"}</button>
+        <button className="button" type="button" onClick={toggleSize}>{isEnlarged ? "Hide" : "Expand"}</button>
       </div>
-      <img className="mx-auto pt-5" src={path}/>
+      <img className="softwareBlurbImg" src={path}/>
     </div>
   )
 }
