56dc9d7f519cedc35a39357b4372e25eb9c93c7e
Addie Lopshire-Bratt <bratt.addie@gmail.com>
2024-01-23 13:11:06 -0700

    description and titles refactored

--- src/SoftwareProjectBlurb.jsx
+++ src/SoftwareProjectBlurb.jsx
@@ -9,10 +9,10 @@ function SoftwareProjectBlurb({ title, role, link, description, path }) {
     setIsEnlarged(!isEnlarged);
   }
   return (
-    <div className={`${isEnlarged ? 'max-h-fit' : 'max-h-[370px]'} overflow-hidden overflow-y-auto shadow-lg p-5 rounded-3xl bg-dark-yellow text-lt-yellow mb-10`}>
-      <h3 className="text-center text-2xl text-bold mb-5">{title}</h3>
-      <p className="text-xl italic mb-3">{role}</p>
-      <p className="mb-3">{description}</p>
+    <div className={`${isEnlarged ? 'softwareBlurbContainerMax' : 'softwareBlurbContainerMin'} softwareBlurbContainer`}>
+      <h3 className="softwareBlurbTitle">{title}</h3>
+      <p className="softwareBlurbRole">{role}</p>
+      <p className="softwareBlurbDescription">{description}</p>
       <div className="relative flex content-end justify-between">
         <button type="button" className="border-2 border-orange text-lt-yellow bg-orange p-4 rounded-xl">
           <a target="_blank" rel="noreferrer" href={link}>Learn more</a>
